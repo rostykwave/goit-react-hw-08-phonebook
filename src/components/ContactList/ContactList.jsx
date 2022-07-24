a5921fe188ade426e8585475b05769ae06c668c5
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-07-24 10:26:52 +0300

    Edit modal

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -12,13 +12,26 @@ import { contactsOperations } from 'redux/contacts';
 import { getVisibleContacts } from 'redux/contacts/contacts-selectors';
 import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
 import EditIcon from '@mui/icons-material/Edit';
+import { useState } from 'react';
+import { EditContact } from 'components/EditContact/EditContact';
 
 export const ContactList = () => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [editPerson, setEditPerson] = useState({
+    id: '',
+    name: '',
+    number: '',
+  });
+  const handleCloseEdit = () => setIsEdit(false);
+
   const contacts = useSelector(getVisibleContacts);
   const dispatch = useDispatch();
   const onDeleteContact = id => dispatch(contactsOperations.deleteContact(id));
-  const onEditContact = id => {
+  const onEditContact = person => {
     console.log('edit');
+    setIsEdit(true);
+    setEditPerson(person);
+
     // dispatch(contactsOperations.deleteContact(id))
   };
 
@@ -34,7 +47,7 @@ export const ContactList = () => {
             {name}: {number}
           </Typography>
           <Box>
-            <IconButton onClick={() => onEditContact(id)}>
+            <IconButton onClick={() => onEditContact({ id, name, number })}>
               <EditIcon />
             </IconButton>
             <IconButton onClick={() => onDeleteContact(id)}>
@@ -43,6 +56,12 @@ export const ContactList = () => {
           </Box>
         </ListItem>
       ))}
+      <EditContact
+        open={isEdit}
+        onClose={handleCloseEdit}
+        editPerson={editPerson}
+      />
+      {/* {isEdit && <EditContact open={isEdit} onClose={handleClose} />} */}
     </List>
   );
 };
